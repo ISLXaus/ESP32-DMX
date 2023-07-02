f73c704bd994e972f742a27bf73281cfb96193af
kokospalme <c.wuepping@gmail.com>
2023-07-02 14:40:17 +0200

    readable

--- src/dmx.h
+++ src/dmx.h
@@ -28,19 +28,15 @@
 #ifndef DMX_h
 #define DMX_h
 
-#define DMX_SERIAL_INPUT_PIN    GPIO_NUM_20 // pin for dmx rx
-#define DMX_SERIAL_OUTPUT_PIN   GPIO_NUM_21 // pin for dmx tx
+#define DMX_SERIAL_INPUT_PIN    GPIO_NUM_20 // pin for dmx rx 20
+#define DMX_SERIAL_OUTPUT_PIN   GPIO_NUM_21 // pin for dmx tx 21
 #define DMX_SERIAL_IO_PIN       GPIO_NUM_1  // pin for dmx rx/tx change
 
 #define DMX_UART_NUM            UART_NUM_1  // dmx uart
-
 #define HEALTHY_TIME            500         // timeout in ms 
-
 #define BUF_SIZE                1024        //  buffer size for rx events
-
 #define DMX_CORE                0           // select the core the rx/tx thread should run on
-
-#define DMX_PRIORITY            2
+#define DMX_PRIORITY            2 
 
 // #define DMX_IGNORE_THREADSAFETY 1           // set to 1 to disable all threadsafe mechanisms
 
